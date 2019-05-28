5ff9c688fc859bdb3ec1fec675f94f254a3b1134
Mario <148373644@qq.com>
2019-05-28 14:43:18 +0800

    update koa koa-static

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const app = new Koa();
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 // static assets
-app.use(koaStatic(path.resolve(__dirname, '..', 'build')));
+app.use(koaStatic(path.resolve(__dirname, '..', 'react-BBS实战/build')));
 
 //异步读取文件的形式
 app.use(async (ctx,next) =>{
   ctx.type = 'html';
-  ctx.body = await fs.createReadStream(path.resolve(__dirname, '..', 'build', 'index.html'));
+  ctx.body = await fs.createReadStream(path.resolve(__dirname, '..', 'react-BBS实战/build', 'index.html'));
 
 })
 
