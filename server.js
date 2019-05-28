2052f5810b633005f4d8c84700d859dbac105b86
Mario <148373644@qq.com>
2019-05-28 13:31:23 +0800

    add server.js ecosystem.json pm2

---
+++ server.js
@@ -0,0 +1,26 @@
+const Koa = require('koa')
+const morgan = require('koa-morgan')
+const path = require('path');
+const koaStatic = require('koa-static')
+const fs = require('fs')
+
+const app = new Koa();
+
+// logger
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+
+// static assets
+app.use(koaStatic(path.resolve(__dirname, '..', 'build')));
+
+//异步读取文件的形式
+app.use(async (ctx,next) =>{
+  ctx.type = 'html';
+  ctx.body = await fs.createReadStream(path.resolve(__dirname, '..', 'build', 'index.html'));
+
+})
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
